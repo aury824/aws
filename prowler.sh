0ccb0c05d08d7101b347b1b50b96dffdbb881b71
aury824 <54835589+aury824@users.noreply.github.com>
2023-05-12 09:58:21 +0900

    Add files via upload

---
+++ prowler.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#Python3.9 install
+wget https://www.python.org/ftp/python/3.9.5/Python-3.9.5.tgz
+tar xvzf Python-3.9.5.tgz
+cd Python-3.9.5
+./configure --enable-optimizations
+sudo make altinstall
+
+HOME_dir=`echo $USER`
+
+echo 'alias python="/usr/local/bin/python3.9"' >> /home/$HOME_dir/.bashrc
+echo 'alias pip="/usr/local/bin/pip3.9"' >> /home/$HOME_dir/.bashrc
+source /home/$HOME_dir/.bashrc
+
+#python -V
+#pip -V
+
+#Prowler3 version install
+pip install prowler
